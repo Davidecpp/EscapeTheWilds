b7704188e3deaa0f266ce0671f918a4a7a0d5d1c
Davidecpp <cppdvd03d13m208q@studenti.unical.it>
2024-09-03 18:18:36 +0200

    enemy health bar

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth = 100.0f;
     private float currentHealth;
+    private PlayerStats _playerStats;
+
+    [SerializeField] private Slider _slider;
 
     void Start()
     {
+        _playerStats = FindObjectOfType<PlayerStats>();
         currentHealth = maxHealth;
     }
 
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
+        UpdateHealthBar();
+        
         Debug.Log("Enemy took damage: " + amount + ", current health: " + currentHealth); 
-
         if (currentHealth <= 0)
         {
             Die();
@@ -24,6 +30,12 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         Debug.Log("Enemy died."); 
+        _playerStats.AddExperience(30);
         Destroy(gameObject);
+    }
+
+    public void UpdateHealthBar()
+    {
+        _slider.value = currentHealth / maxHealth;
     }
 }
