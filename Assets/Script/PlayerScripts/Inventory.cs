6b341d6807c4ccf342531b6c0a5a2edfb9ab915a
Davidecpp <cppdvd03d13m208q@studenti.unical.it>
2024-11-15 01:24:09 +0100

    inventory comments

--- Assets/Script/PlayerScripts/Inventory.cs
+++ Assets/Script/PlayerScripts/Inventory.cs
@@ -1,99 +1,117 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using Unity.VisualScripting;
 
 public class Inventory : MonoBehaviour
 {   
-    // Key
+    // Key: The player's status regarding key possession
     public bool hasKey = false;
+    
+    // Max bullet capacity
     public int maxBullets = 10;
+    
+    // Reference to the CanvasManager for UI updates
     private CanvasManager _canvasManager;
 
-    [System.Serializable] // Makes resource counters serializable
+    [System.Serializable] // Makes resource counters serializable so they can be displayed in the inspector
     public struct ResourceCounter
     {
-        public int count;
-        public TextMeshProUGUI counterText;
+        public int count;  // The current count of the resource
+        public TextMeshProUGUI counterText; // The UI text element to display the resource count
     }
+
+    // Start is called before the first frame update
     private void Start()
     {
+        // Find and store the CanvasManager instance
         _canvasManager = FindObjectOfType<CanvasManager>();
+
+        // Initialize bullet count display
         SetBullets(_canvasManager.bulletCounter);
 
+        // If the current scene is 4 (e.g., a specific game level), set coin count to 0
         if (GameManager.Instance.currentScene == 4)
         {
             SetCoinCount(0);
         }
     }
 
+    // Set the bullet counter UI display with the current count and max bullets
     private void SetBullets(ResourceCounter resourceCounter)
     {
+        // Check if the counterText is set and update the bullet count display
         if (resourceCounter.counterText != null && resourceCounter.Equals(_canvasManager.bulletCounter))
         {
             resourceCounter.counterText.text = "x " + resourceCounter.count + "/" + maxBullets;
         }
     }
 
+    // Add a certain amount of resources to a given resource counter (e.g., bullets, coins)
     public void AddResource(ref ResourceCounter resourceCounter, int amount)
     {
         for (int i = 0; i < amount; i++)
         {
+            // Check if it's the bullet counter and if the max bullet capacity is reached
             if (resourceCounter.Equals(_canvasManager.bulletCounter) && resourceCounter.count >= maxBullets)
             {
-                Debug.Log("MAX AMMO");
+                Debug.Log("MAX AMMO");  // Log message if the max bullets is reached
             }
             else
             {
+                // Increment the resource count and update the UI
                 resourceCounter.count++;
                 UpdateResourceCounter(resourceCounter);
             }
         }
     }
 
+    // Update the resource counter's UI text
     private void UpdateResourceCounter(ResourceCounter resourceCounter)
     {
+        // Check if the counterText is not null and update the display
         if (resourceCounter.counterText != null)
         {
             resourceCounter.counterText.text = "x " + resourceCounter.count;
+            // If it's the bullet counter, also display the max bullets
             if (resourceCounter.Equals(_canvasManager.bulletCounter))
             {
-                resourceCounter.counterText.text += "/"+maxBullets;
+                resourceCounter.counterText.text += "/" + maxBullets;
             }
         }
         else
         {
-            Debug.LogError("Resource Counter Text reference is not set.");
+            Debug.LogError("Resource Counter Text reference is not set.");  // Log an error if the counterText is not set
         }
     }
 
+    // Get the current resource count for a specific resource counter
     public int GetResourceCount(ResourceCounter resourceCounter)
     {
         return resourceCounter.count;
     }
 
+    // Get the current bullet count
     public int GetBullets()
     {
         return _canvasManager.bulletCounter.count;
     }
 
+    // Decrease the bullet count by one and update the UI
     public void RemoveBullet()
     {
         _canvasManager.bulletCounter.count--;
         _canvasManager.bulletCounter.counterText.text = "x " + _canvasManager.bulletCounter.count + "/" + maxBullets;
-        
     }
 
+    // Decrease the coin count by a specified amount and update the UI
     public void RemoveCoins(int amount)
     {
         _canvasManager.coinCounter.count -= amount;
         _canvasManager.coinCounter.counterText.text = "x " + _canvasManager.coinCounter.count;
     }
 
+    // Set the coin count to a specific amount and update the UI
     public void SetCoinCount(int amount)
     {
-        Debug.Log("Coin set to "+ amount);
         _canvasManager.coinCounter.count = amount;
         UpdateResourceCounter(_canvasManager.coinCounter);
     }
